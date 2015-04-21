6f813af39a902a6fa8bafe2e18e05ff7661bf686
ethan <dirtdirtdirtdirt@aol.com>
2015-04-21 17:29:26 -0400

    Initial Commit

---
+++ textFieldTryouts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  textFieldTryouts
+//
+//  Created by Ethan Haley on 4/21/15.
+//  Copyright (c) 2015 Ethan Haley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
